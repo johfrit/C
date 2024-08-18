f8f42de350e72b161c8cb12565e4a5a1c8cd5994
johfrit <johfrit2004@gmail.com>
2024-08-18 13:14:42 +0530

    sum of arrays

---
+++ code1.c
@@ -0,0 +1,15 @@
+//sum of array-very big number
+#include<stdio.h> 
+int main()
+{
+    int n,i;
+    long sum=0;
+    scanf("%d",&n);
+    long arr[n];
+    for(i=0;i<n;i++){
+        scanf("%ld",&arr[i]);
+        sum+=arr[i];
+    }
+    printf("%ld",sum);
+    return 0;
+}
